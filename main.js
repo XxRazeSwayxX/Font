5bf1eca981e8dcd87b12c4238e24536bab757478
XxRaZes_SwayxX <97645739+XxRazeSwayxX@users.noreply.github.com>
2022-09-12 18:32:50 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560,150);
+    poseNet = ml5.poseNet(video, modelloaded)
+    poseNet.on("pose", gotposes)
+}
+
+function modelloaded() {
+    console.log("PoseNet is initialized")
+}
+
+function gotposes(results) {
+    if(results.length > 0) {
+        console.log(results)
+    }
+}
+
+function draw() {
+    background("#679267")
+}
+
